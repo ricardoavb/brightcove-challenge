e55b22eb27146259b86e0a98ae4835dd84a80ea2
ricardo <dafunk61@gmail.com>
2022-11-16 15:38:08 -0600

    using data provider

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@playwright/test'
 import { HomePage } from '../pages/home-page'
+import { name } from '../data/data-provider'
 
 
 test('submit button is disabled without a value for name', async ({ page }) => {
@@ -12,6 +13,6 @@ test('submit form details', async ({ page }) => {
   const homePage = new HomePage(page)
   await homePage.goto()
   await homePage.populateName()
-  await homePage.enterName('test name')
+  await homePage.enterName(name)
   await homePage.checkFeatures()
 })
