a89ad2dd4f60bf483ca5f6ee57f898dc71952ac4
ricardo <dafunk61@gmail.com>
2022-11-16 15:24:13 -0600

    add page methods

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,12 +1,17 @@
-import { Locator, Page } from '@playwright/test'
+import { expect, Locator, Page } from '@playwright/test'
+
 
 export class HomePage {
   readonly page: Page
   readonly nameInput: Locator
+  readonly submitButton: Locator
+  readonly populateButton: Locator
 
   constructor(page: Page) {
     this.page = page;
     this.nameInput = page.locator('#developer-name')
+    this.submitButton = page.locator('#submit-button')
+    this.populateButton = page.locator('#populate')
   }
 
   async goto() {
@@ -15,5 +20,26 @@ export class HomePage {
 
   async enterName(name: string) {
     await this.nameInput.fill(name)
+  }
+
+  async verifySubmitButton() {
+    await expect(this.submitButton).toBeDisabled()
+  }
+
+  async populateName() {
+    await this.populateButton.click()    
+  }
+
+  async checkFeatures() {
+    const featureIds: string[] = [
+        'remote-testing',
+        'reusing-js-code',
+        'background-parallel-testing'
+    ]
+
+    featureIds.forEach(async featureId => {
+        const locator = this.page.locator(`#${featureId}`)
+        await locator.click()
+    })
   }
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,8 +2,15 @@ import { test } from '@playwright/test'
 import { HomePage } from '../pages/home-page'
 
 
-test('first test', async ({ page }) => {
+test('submit button is disabled without a value for name', async ({ page }) => {
   const homePage = new HomePage(page)
   await homePage.goto()
-  await homePage.enterName('test')
+  await homePage.verifySubmitButton()
+})
+
+test('submit form details', async ({ page }) => {
+  const homePage = new HomePage(page)
+  await homePage.goto()
+  await homePage.populateName()
+  await homePage.enterName('test name')
 })
